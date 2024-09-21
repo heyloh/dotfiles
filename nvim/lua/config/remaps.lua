30fbd3d1548e3bf32f831b699026a81e527533be
Lohana Torres <lt.leopoldo@gmail.com>
2024-09-21 09:11:24 -0300

    feat: add new remaps and tmux-navigator plugin

--- nvim/lua/config/remaps.lua
+++ nvim/lua/config/remaps.lua
@@ -1,3 +1,13 @@
 local keymap = vim.keymap
 
 keymap.set("n", "<C-s>", "<cmd>w<CR>")
+
+keymap.set("v", "J", ":m'>+<CR>gv=gv")
+keymap.set("v", "K", ":m-2<CR>gv=gv")
+
+keymap.set("v", "<leader>y", '"+y')
+
+-- keymap.set("n", "<C-h>", "<C-w>h")
+-- keymap.set("n", "<C-j>", "<C-w>j")
+-- keymap.set("n", "<C-k>", "<C-w>k")
+-- keymap.set("n", "<C-l>", "<C-w>l")
