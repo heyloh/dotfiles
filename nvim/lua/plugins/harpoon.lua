fe635e4cbf21c6f50c9a71c1a0a6398166fc8cfe
Lohana Torres <lt.leopoldo@gmail.com>
2025-01-21 21:29:20 -0300

    feat: replaces telescope with fzf-lua

--- nvim/lua/plugins/harpoon.lua
+++ nvim/lua/plugins/harpoon.lua
@@ -5,25 +5,25 @@ return {
 	opts = {},
 	config = function()
 		local harpoon = require("harpoon")
-		local conf = require("telescope.config").values
+		-- local conf = require("telescope.config").values
 
-		local function toggle_telescope(harpoon_files)
-			local file_paths = {}
-			for _, item in ipairs(harpoon_files.items) do
-				table.insert(file_paths, item.value)
-			end
-
-			require("telescope.pickers")
-				.new({}, {
-					prompt_title = "Harpoon",
-					finder = require("telescope.finders").new_table({
-						results = file_paths,
-					}),
-					previewer = conf.file_previewer({}),
-					sorter = conf.generic_sorter({}),
-				})
-				:find()
-		end
+		-- local function toggle_telescope(harpoon_files)
+		-- 	local file_paths = {}
+		-- 	for _, item in ipairs(harpoon_files.items) do
+		-- 		table.insert(file_paths, item.value)
+		-- 	end
+		--
+		-- 	require("telescope.pickers")
+		-- 		.new({}, {
+		-- 			prompt_title = "Harpoon",
+		-- 			finder = require("telescope.finders").new_table({
+		-- 				results = file_paths,
+		-- 			}),
+		-- 			previewer = conf.file_previewer({}),
+		-- 			sorter = conf.generic_sorter({}),
+		-- 		})
+		-- 		:find()
+		-- end
 
 		local wk = require("which-key")
 		wk.add({
@@ -37,8 +37,8 @@ return {
 			{
 				"<leader>hl",
 				function()
-					toggle_telescope(harpoon:list())
-					-- harpoon.ui:toggle_quick_menu(harpoon:list())
+					-- toggle_telescope(harpoon:list())
+					harpoon.ui:toggle_quick_menu(harpoon:list())
 				end,
 				desc = "Open harpoon list",
 			},
